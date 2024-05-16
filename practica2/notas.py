dcc3c4725f80281d7de9a882161c7ac17cba6015
willyHuamali <47932688+willyHuamali@users.noreply.github.com>
2024-05-16 17:41:59 -0500

    ejercicios 1 y 3 ya subidos

--- practica2/notas.py
+++ practica2/notas.py
@@ -1,2 +1,3 @@
 """En esta carpeta adjuntaremos nuestros ejercicios y su rama debe estar basada
 en esta rama por favor. Gracias ;)"""
+""" ya subi el ejercicio 1 y 3  confirmen si lo pueden visualizar""" 
