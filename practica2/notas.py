784e6534a9889c732133014bc4f6310dfd02c046
willyHuamali <willyhuamali@gmail.com>
2024-05-18 10:25:20 -0500

    ultimos cambios en la carpeta 1

--- practica2/notas.py
+++ practica2/notas.py
@@ -1,3 +1,3 @@
 """En esta carpeta adjuntaremos nuestros ejercicios y su rama debe estar basada
 en esta rama por favor. Gracias ;)"""
-"""se subio los ejercioos de la semana 1"""
+"""se subio los ejercicios de la semana 1"""
